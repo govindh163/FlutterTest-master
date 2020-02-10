56159ebf2517298705b2f7c637e8efe4c66e5328
govindharajan <govindharajan@smartiapps.com>
2020-02-10 12:46:14 +0530

    notification with progress

--- lib/Screens/localnotification.dart
+++ lib/Screens/localnotification.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
+import 'dart:io';
 import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-
+import 'package:path_provider/path_provider.dart';
+import 'package:http/http.dart' as http;
 class LocalNotify extends StatefulWidget {
   @override
   _LocalNotifyState createState() => _LocalNotifyState();
@@ -12,8 +14,10 @@ class _LocalNotifyState extends State<LocalNotify> {
   FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
    String time=DateTime.now().toString();
   @override
+
   void initState() {
     super.initState();
+    _scheduleNotification();
     flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
     var android = new AndroidInitializationSettings('@mipmap/ic_launcher');
     var iOS = new IOSInitializationSettings();
@@ -43,25 +47,51 @@ class _LocalNotifyState extends State<LocalNotify> {
         icon:Icon(Icons.ac_unit),
       ),),
 
-      body: new Center(
-        child: new RaisedButton(
-          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 35),
-          shape: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(35),
-            borderSide: BorderSide.none,
+      body: Column(
+        children: <Widget>[
+          new Center(
+            child: new RaisedButton(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 35),
+              shape: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(35),
+                borderSide: BorderSide.none,
+              ),
+              onPressed: showNotification,
+              child: new Text(
+                'Click to get Notification ',
+                style: Theme.of(context).textTheme.headline,
+              ),
+            ),
           ),
-          onPressed: showNotification,
-          child: new Text(
-            'Click to get Notification ',
-            style: Theme.of(context).textTheme.headline,
+     SizedBox(height: 20,),
+     RaisedButton(
+       child: new Text(
+         'Click to get Image Notification ',
+         style: Theme.of(context).textTheme.headline,
+       ),
+            onPressed: () async {
+              await _showBigPictureNotification();
+            },
           ),
-        ),
+          SizedBox(height: 20,),
+          RaisedButton(
+            child: new Text(
+              'Click to  get loading Notification ',
+              style: Theme.of(context).textTheme.headline,
+            ),
+            onPressed: () async {
+              await  _showIndeterminateProgressNotification();
+            },
+          ),
+        ],
       ),
     );
   }
   Future<void> _scheduleNotification() async {
     var scheduledNotificationDateTime =
     DateTime.now().add(Duration(seconds: 1));
+
+
 
     var androidPlatformChannelSpecifics = AndroidNotificationDetails(
         'your other channel id',
@@ -87,7 +117,82 @@ class _LocalNotifyState extends State<LocalNotify> {
       payload:"You achieved Great",);
 
   }
-
+  Future<void> _showIndeterminateProgressNotification() async {
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        'indeterminate progress channel',
+        'indeterminate progress channel',
+        'indeterminate progress channel description',
+        channelShowBadge: false,
+        importance: Importance.Max,
+        priority: Priority.High,
+        onlyAlertOnce: true,
+        showProgress: true,
+        indeterminate: true);
+    var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+        0,
+        'indeterminate progress notification title',
+        'indeterminate progress notification body',
+        platformChannelSpecifics,
+        payload: 'item x');
+  }
+  String _toTwoDigitString(int value) {
+    return value.toString().padLeft(2, '0');
+  }
+  Future<void> _showDailyAtTime() async {
+    var time = Time(12, 36, 0);
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        'repeatDailyAtTime channel id',
+        'repeatDailyAtTime channel name',
+        'repeatDailyAtTime description');
+    var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.showDailyAtTime(
+        0,
+        'show daily title',
+        'Daily notification shown at approximately ${_toTwoDigitString(time.hour)}:${_toTwoDigitString(time.minute)}:${_toTwoDigitString(time.second)}',
+        time,
+        platformChannelSpecifics);
+  }
+  Future<String> _downloadAndSaveImage(String url, String fileName) async {
+    var directory = await getApplicationDocumentsDirectory();
+    var filePath = '${directory.path}/$fileName';
+    var response = await http.get(url);
+    var file = File(filePath);
+    await file.writeAsBytes(response.bodyBytes);
+    return filePath;
+  }
+  Future<void> _showBigPictureNotification() async {
+    var largeIconPath = await _downloadAndSaveImage(
+        'http://via.placeholder.com/48x48', 'largeIcon');
+    var bigPicturePath = await _downloadAndSaveImage(
+        'http://via.placeholder.com/400x800', 'bigPicture');
+    var bigPictureStyleInformation = BigPictureStyleInformation(
+        bigPicturePath, BitmapSource.FilePath,
+        largeIcon: largeIconPath,
+        largeIconBitmapSource: BitmapSource.FilePath,
+        contentTitle: 'overridden <b>big</b> content title',
+        htmlFormatContentTitle: true,
+        summaryText: 'summary <i>text</i>',
+        htmlFormatSummaryText: true);
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        'big text channel id',
+        'big text channel name',
+        'big text channel description',
+        playSound: true,
+        enableLights: true,
+        color: const Color.fromARGB(255, 255, 0, 0),
+        ledColor: const Color.fromARGB(255, 255, 0, 0),
+        style: AndroidNotificationStyle.BigPicture,
+        styleInformation: bigPictureStyleInformation);
+    var platformChannelSpecifics =
+    NotificationDetails(androidPlatformChannelSpecifics, null);
+    await flutterLocalNotificationsPlugin.show(
+        0, 'big text title', 'silent body', platformChannelSpecifics);
+  }
   showNotification() async {
     var android = new AndroidNotificationDetails(
         'channel id', 'channel NAME', 'CHANNEL DESCRIPTION',
